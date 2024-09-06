652888bc774cd745780e80164eb87f0dd6bde00e
Alexander carvajal <alexcarvajalcontacto@gmail.com>
2024-09-06 14:12:26 -0300

    Agregados los archivos de la tarea de la clase 5

---
+++ Actividad5/TareaBootstrap/index.js
@@ -0,0 +1,51 @@
+let titulo = document.getElementById("titulo")
+let btnSubmit = document.getElementById("btn-submit")
+let formInputs = document.querySelectorAll(".form-control");
+let formGroup = document.querySelectorAll(".form-group");
+let textoFecha = document.getElementById("fecha")
+
+function estiloContraste(){
+    //fondo pagina
+    document.body.classList.add("bg-dark")
+
+    //textos
+    titulo.classList.add("text-light")
+    fecha.classList.add("text-light")
+
+    //boton submit
+    btnSubmit.classList.add("btn-warning");
+
+    //clases input y form-group
+    formInputs.forEach(elem => {
+        elem.classList.add("bg-dark");
+    });
+    formGroup.forEach(elem => {
+        elem.classList.add("text-light")
+    })
+}
+
+function estiloNormal(){
+    //fondo pagina
+    document.body.classList.remove("bg-dark")
+
+    //textos
+    titulo.classList.remove("text-light")
+    fecha.classList.remove("text-light")
+
+    //boton submit
+    btnSubmit.classList.remove("btn-warning");
+    
+    //clases input y form-group
+    formInputs.forEach(elem => {
+        elem.classList.remove("bg-dark");
+    });
+    formGroup.forEach(elem => {
+        elem.classList.remove("text-light")
+    })
+}
+
+let btnChange = document.getElementById("btn-contraste")
+btnChange.addEventListener("click", estiloContraste)
+
+let btnNormal = document.getElementById("btn-normal")
+btnNormal.addEventListener("click", estiloNormal)
